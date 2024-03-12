cbe3007fac3c176e352c445aec7713c193c35d90
Vermaanug <Vermaanug1974@gmail.com>
2024-03-12 12:03:22 +0530

    Add Component folder with Header, Body, Browse, and Login components

---
+++ Netflix-Frontend/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="relative w-8/12 m-auto bg-red-600">
+      <div className="absolute left-0 top-0 px-8 py-2 z-10">
+        <img
+          className="w-48"
+          src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+          alt="logo"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
