1b1d364cf7042bc08b1131ea686a479d0b0b0587
Vermaanug <Vermaanug1974@gmail.com>
2024-03-12 12:13:04 +0530

    Implemented form validation for email and password fields

---
+++ Netflix-Frontend/src/utils/Validate.js
@@ -0,0 +1,15 @@
+export const checkValidData = (email, password) => {
+  const isEmailValid = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(
+    email
+  );
+  const isPasswordValid =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(
+      password
+    );
+
+  if (!isEmailValid) return "Enter a valid email id";
+  if (!isPasswordValid)
+    return "Password should contain at least one uppercase letter, one lowercase letter, one number and one special character ";
+
+  return null;
+};
