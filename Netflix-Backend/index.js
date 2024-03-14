aac9d3d2eff153383e571e831c49f506bae2150c
Vermaanug <Vermaanug1974@gmail.com>
2024-03-14 15:04:09 +0530

    Include middleware and user route handling

--- Netflix-Backend/index.js
+++ Netflix-Backend/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
+require("./utilis/connection");
+const cookieParser = require("cookie-parser");
+const userRoute = require("./routes/userRoute");
 const app = express();
 const port = process.env.PORT || 8000;
+
+//MiddleWares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+//api
+app.use("/api/vi/user", userRoute);
+
 
 app.listen(port, () => {
   console.log(`Listening on Port ${port}`);
