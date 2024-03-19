702a184da5d95fc94e35610c4c20e3fdaeb000c2
Vermaanug <Vermaanug1974@gmail.com>
2024-03-19 13:04:03 +0530

    Integrate Redux, navigation logic, and useNowPlayingMovies hook

--- Netflix-Frontend/src/components/Browse.jsx
+++ Netflix-Frontend/src/components/Browse.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Header from "./Header";
+import { useSelector } from "react-redux";
+import appStore from "../redux/appStore";
+import { useNavigate } from "react-router-dom";
+import useNowPlayingMovies from "../../hooks/useNowPlayingMovies";
 
 const Browse = () => {
-  return (
-    <div>Browse</div>
-  )
-}
+  const currentUser = useSelector((appStore) => appStore.user.currentUser);
+  const navigate = useNavigate();
+
+  useNowPlayingMovies();  //  Custom Hooks to fetching data from API and store it in Redux Store
+
+  useEffect(() => {
+    if (!currentUser) {
+      navigate("/");
+    }
+  }, []);
+
+  return <Header />;
+};
 
 export default Browse;
