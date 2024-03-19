0f714ce12035d0e5e6414fe2464ae8378b6cc34d
Vermaanug <Vermaanug1974@gmail.com>
2024-03-19 12:58:09 +0530

    Created a Spinner Component

---
+++ Netflix-Frontend/src/components/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="spinner border-t-2 border-gray-200 rounded-full w-8 h-8 animate-spin"></div>
+    </div>
+  );
+};
+
+export default Spinner;
