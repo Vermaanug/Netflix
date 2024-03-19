3252bdf451547878ff02e30c6da0408fceea5ac6
Vermaanug <Vermaanug1974@gmail.com>
2024-03-19 12:58:52 +0530

    Implemented Sign out Functionality

--- Netflix-Frontend/src/components/Header.jsx
+++ Netflix-Frontend/src/components/Header.jsx
@@ -1,22 +1,47 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import appStore from "../redux/appStore";
+import axios from "axios";
+import { API_END_POINT, LOGO } from "../utils/constant";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { removeUser } from "../redux/userSlice";
 
 const Header = () => {
   const currentUser = useSelector((appStore) => appStore.user.currentUser);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleSignOut = async () => {
+    try {
+      const res = await axios.get(`${API_END_POINT}/logout`);
+      if (res.data.success) {
+        dispatch(removeUser());
+        navigate("/");
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return currentUser ? (
     <div className="w-full m-auto bg-gradient-to-b from-black flex justify-around">
       <div className="py-2">
         <img
           className="w-36"
-          src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+          src={LOGO}
           alt="logo"
         />
       </div>
       <div className="flex justify-between">
         <h1 className="my-5 text-xl">{currentUser.FullName}</h1>
-        <button className="ml-6 px-2 mt-4 mb-6 bg-red-600 rounded-sm text-white text-sm font-semibold cursor-pointer">Sign Out</button>
+        <button
+          className="ml-6 px-2 mt-4 mb-6 bg-red-600 rounded-sm text-white text-sm font-semibold cursor-pointer"
+          onClick={handleSignOut}
+        >
+          Sign Out
+        </button>
       </div>
     </div>
   ) : (
@@ -24,7 +49,7 @@ const Header = () => {
       <div className="absolute left-0 top-0 px-8 py-2 z-10">
         <img
           className="w-48"
-          src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+          src={LOGO}
           alt="logo"
         />
       </div>
