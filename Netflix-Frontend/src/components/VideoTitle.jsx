e862534ea2093fc69fb503d11684907ace360825
Vermaanug <Vermaanug1974@gmail.com>
2024-03-20 14:25:15 +0530

    Install react-icons and created VideoTitle Component

---
+++ Netflix-Frontend/src/components/VideoTitle.jsx
@@ -0,0 +1,22 @@
+import { FaPlay } from "react-icons/fa";
+import { CiCircleInfo } from "react-icons/ci";
+const VideoTitle = ({ title, overview }) => {
+  return (
+    <div className="pt-80 px-36">
+      <h1 className="text-4xl font-semibold mb-2">{title}</h1>
+      <span className="text-lg inline-block w-5/12 mb-4">{overview}</span>
+      <div className="flex">
+        <button className="bg-red-600 text-white px-8 py-3 text-xl flex items-center font-semibold rounded-md cursor-pointer bg-opacity-60 mr-5">
+          <FaPlay className="text-lg mt-1 mr-1" />
+          Play
+        </button>
+        <button className="bg-red-600 text-white px-8 py-3 text-xl flex items-center font-semibold rounded-md cursor-pointer bg-opacity-60 mr-5">
+          <CiCircleInfo className="text-2xl mt-1 mr-1" />
+          More Info
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default VideoTitle;
