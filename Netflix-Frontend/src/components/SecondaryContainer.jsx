32b8c291adac01c6a12cd76a3f0dc75ac163f384
Vermaanug <Vermaanug1974@gmail.com>
2024-03-26 18:59:31 +0530

    Updated SecondaryContainer to contain MovieList Component

--- Netflix-Frontend/src/components/SecondaryContainer.jsx
+++ Netflix-Frontend/src/components/SecondaryContainer.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
+import appStore from "../redux/appStore";
 
 const SecondaryContainer = () => {
+  const movies = useSelector((appStore) => appStore.movies.nowPlayingMovies);
+  console.log(movies);
+
   return (
-    <div>SecondaryContainer</div>
-  )
-}
+    <div className="bg-black">
+      <div className="-mt-52 relative z-10">
+        <MovieList title={"Now Playing"} movies={movies} />
+        <MovieList title={"Now Playing"} movies={movies} />
+      </div>
+    </div>
+  );
+};
 
 export default SecondaryContainer;
