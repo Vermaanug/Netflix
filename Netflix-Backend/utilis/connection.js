571d0a4b2e4105be67d529e0c8ffc0257c1ee704
Vermaanug <Vermaanug1974@gmail.com>
2024-04-02 17:33:23 +0530

    Updated MongoDB connection to use environment variables

--- Netflix-Backend/utilis/connection.js
+++ Netflix-Backend/utilis/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose
-  .connect("mongodb+srv://vermaanug1974:Dj5DLSdCP5Zg2nOb@cluster0.9es6z86.mongodb.net/")
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("Connection Successful");
   })
